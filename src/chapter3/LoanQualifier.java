c2000004ae9530457dc00a70e49f572e6341d6ca
Hector Grajales Orozco <hagrajales87@gmail.com>
2022-10-06 14:56:08 -0500

    Chapter 3c Nested If Statements

---
+++ src/chapter3/LoanQualifier.java
@@ -0,0 +1,43 @@
+package chapter3;
+
+/*
+    NESTED IFS:
+    To qualify for a loan, a person must take at least $30,000
+    and have been working at their current job for at least 2 years
+ */
+
+import java.util.Scanner;
+
+public class LoanQualifier {
+
+    public static void main (String args[]){
+
+        //Initialize what we know
+        int requiredSalary = 30000;
+        int requiredYearEmployed = 2;
+
+        // Get what we don't
+        System.out.println("Enter your Salary");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter the number of years with your current employer");
+        double years = scanner.nextDouble();
+
+        scanner.close();
+
+        //Make decision
+        if(salary >= requiredSalary){
+            if(years >= requiredYearEmployed){
+                System.out.println("Congrats! You qualify for the loan");
+            }
+            else{
+                System.out.println("Sorry, you must have worked ar your current job " +requiredYearEmployed + " years.");
+            }
+        }
+        else {
+            System.out.println("Sorry, you must earn at least $" + requiredSalary + " to qualify for the loan");
+        }
+
+    }
+}
