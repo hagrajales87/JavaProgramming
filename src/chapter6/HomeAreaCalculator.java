75e04853722b1142764b0729c96a4623a916a857
Hector Grajales Orozco <hagrajales87@gmail.com>
2022-10-08 21:56:06 -0500

    Instantiate objects and Objects as Method parameters and return types

---
+++ src/chapter6/HomeAreaCalculator.java
@@ -0,0 +1,31 @@
+package chapter6;
+
+/*
+    Write a class that creates instances of the Rectangle class to find the
+    total area of two rooms in a house.
+ */
+public class HomeAreaCalculator {
+    public static void main(String args[]){
+        /************
+         * RECTANGLE 1
+         ************/
+
+        // Create instance of Rectangles class
+        Rectangle room1 = new Rectangle();
+        room1.setLength(3);
+        room1.setWidth(3);
+        double areaOfRoom1 = room1.calculateArea();
+        System.out.println("The first room area: "+areaOfRoom1);
+
+        /************
+         * RECTANGLE 2
+         ************/
+        // Create instance of Rectangles class
+        Rectangle room2 = new Rectangle(4, 4);
+        double areaOfRoom2 = room2.calculateArea();
+        System.out.println("The first room area: "+areaOfRoom2);
+
+        double totalArea = areaOfRoom1 + areaOfRoom2;
+        System.out.println("Area of both rooms: "+ totalArea);
+    }
+}
