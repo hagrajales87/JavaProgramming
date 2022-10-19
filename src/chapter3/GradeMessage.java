74769ff082d48daf98643fbc3c0fbe60d564e141
Hector Grajales <hagrajales87@gmail.com>
2022-10-18 21:35:47 -0500

    An small improvement to the Switch Class GradeMessage

--- src/chapter3/GradeMessage.java
+++ src/chapter3/GradeMessage.java
@@ -8,30 +8,36 @@ public class GradeMessage {
 
         System.out.println("Enter your letter grade:");
         Scanner scanner = new Scanner(System.in);
-        String grade = scanner.next();
 
         String message;
+        String grade;
+        do {
+            grade = scanner.next();
 
-        switch(grade){
-            case "A":
-                message= "Excelent job!";
-                break;
-            case "B":
-                message= "Great job!";
-                break;
-            case "C":
-                message= "God job!";
-                break;
-            case "D":
-                message = "You need to work a bit harder";
-                break;
-            case "F":
-                message = "Uh ho!";
-                break;
-            default:
-                message = "Error. Invalid grade";
-                break;
-        }
+            switch (grade) {
+                case "A":
+                    message = "Excelent job!";
+                    break;
+                case "B":
+                    message = "Great job!";
+                    break;
+                case "C":
+                    message = "God job!";
+                    break;
+                case "D":
+                    message = "You need to work a bit harder";
+                    break;
+                case "F":
+                    message = "Uh ho!";
+                    break;
+                default:
+                    message = "Error. Invalid grade";
+                    System.out.println("Error. Invalid grade, Try Again");
+                    break;
+            }
+        }while(message.equalsIgnoreCase("Error. Invalid grade"));
+
+        System.out.println(message.equalsIgnoreCase(grade));
         System.out.println("Your grade is " + grade);
         System.out.println(message);
     }
